4754263826f6ea5624c6badc8a5813736da16fcf
piyush_lasane <160647609+piyushlasane@users.noreply.github.com>
2024-07-25 23:42:59 +0530

    Update GuessingGame.c

--- GuessingGame.c
+++ GuessingGame.c
@@ -4,12 +4,9 @@
 
 int main(){
     int number, guess, nguesses = 1;
-    
     srand(time(0));
     number = rand() % 100 + 1;
-    
     printf("Guess the number between 1 to 100\n");
-
     do{
         scanf("%d", &guess);
         if (guess > number){
