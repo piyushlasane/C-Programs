1fa8e1ebe209842b7440fbf2c8741ab708594875
piyush_lasane <160647609+piyushlasane@users.noreply.github.com>
2024-08-09 21:23:44 +0530

    Update FibonacciSeries.c

--- FibonacciSeries.c
+++ FibonacciSeries.c
@@ -1,29 +1,22 @@
 #include <stdio.h>
-int fib_recursive(int n)
-{
-    if (n==1||n==2)
-    {
+int fib_recursive(int n){
+    if (n==1||n==2){
         return n-1;
-    }
-    else
-    {
+    } else {
         return fib_recursive(n-1)+fib_recursive(n-2);
     }
 }
-int fib_iterative(int n)
-{
+int fib_iterative(int n){
     int a=0;
     int b=1;
-    for (int i = 0; i < n-1; i++)
-    {
+    for (int i = 0; i < n-1; i++){
         b=a+b;
         a=b-a;
     }
     
     return a;
 }
-int main()
-{
+int main(){
     int number;
     printf("Enter the index to find fibonacci series\n");
     scanf("%d", &number);
