560a5253ec0c0016409469385fdc89fc1a7d91d7
piyush_lasane <160647609+piyushlasane@users.noreply.github.com>
2024-08-09 21:24:39 +0530

    Update FibonacciSeries.c

--- FibonacciSeries.c
+++ FibonacciSeries.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-int fib_recursive(int n){
-    if (n==1||n==2){
-        return n-1;
-    } else {
-        return fib_recursive(n-1)+fib_recursive(n-2);
-    }
-}
+// int fib_recursive(int n){
+//     if (n==1||n==2){
+//         return n-1;
+//     }
+//     return fib_recursive(n-1)+fib_recursive(n-2);
+// }
+
 int fib_iterative(int n){
     int a=0;
     int b=1;
